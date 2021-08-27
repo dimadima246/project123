dc0015cce2cba493b755704a6a513052dfcf80c0
Dima <dimadubrovy@mail.ru>
2021-08-27 20:18:04 +0300

    Условия

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 "use strict";
-
+// .................................................1 задание
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-
+//.................................................2 задание
 const personalMovieDB = {
         count: numberOfFilms,
         movies:{},
@@ -10,7 +10,7 @@ const personalMovieDB = {
         privat: false
 };
 
-//3 задание
+//.............................................3 задание
 
 const a = prompt("Один из просмотренных фильмов", ''),
       b = prompt("На сколько оцените его?", ''),
@@ -22,3 +22,43 @@ personalMovieDB.movies[a] = b;
 personalMovieDB.movies[c] = d;
 
 console.log(personalMovieDB);
+
+
+//..............................................Условия
+
+if (4==4) {
+        console.log('ok');
+} else {
+        console.log('error');
+}
+
+
+
+// if(num < 49) {
+//         console.log('error');
+// } else if (num > 100) {
+//         console.log('many')
+// } else {
+//         console.log('ok')
+// }
+
+// (num === 50) ?console.log('ok') : console.log('error');//на тернарном
+
+const num =50;
+switch (num) {
+        case 49:
+                console.log('неверно');
+                break;
+        case 100:
+                console.log('неверно');
+                break;
+        case 50: 
+                console.log('great');
+                break;
+
+        default:
+                console.log('не в этот раз');
+                break
+
+
+}
